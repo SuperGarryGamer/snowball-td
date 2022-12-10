1f15e8c32fb10063a2ab1d8bc05c694d669b88d5
SuperGarryGamer <supergarrygamer@gmail.com>
2022-12-10 12:08:37 +0100

    add add_obj method

--- Burglar.py
+++ Burglar.py
@@ -1,7 +1,6 @@
 import GameObject
 
 class Burglar(GameObject):
-
     def __init__(self, game, x, y, w, h, img_path, visible=True, collision=True, damage=10):
         super.__init__(game, x, y, w, h, img_path, visible, collision)
         self.damage = damage
--- Scene.py
+++ Scene.py
@@ -3,5 +3,8 @@ import GameObject
 class Scene:
     objects = {}
 
+    def add_obj(self, id, obj):
+        self.objects[id] = obj
+
     def __init__(self, objects = {}):
         self.objects = objects
